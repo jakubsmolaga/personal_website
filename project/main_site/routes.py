5ef5a4ebe5277a4850c07c015e126f4528d81f19
smolagakuba <smolagakuba@gmail.com>
2019-06-13 01:58:01 +0200

    favicon + box shadow

--- project/main_site/routes.py
+++ project/main_site/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 mod = Blueprint('main_page', __name__, template_folder='templates')
 
@@ -13,3 +13,6 @@ def contact():
 @mod.route('/about')
 def about():
     return render_template('main_site/about.html')
+@mod.route('/favicon.ico')
+def favicon():
+    return redirect('/static/img/favicon.ico')
