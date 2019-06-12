accc34229b1ddb67419cfd123ac600bcfa06edad
smolagakuba <smolagakuba@gmail.com>
2019-06-13 01:32:42 +0200

    visual update

--- project/main_site/routes.py
+++ project/main_site/routes.py
@@ -5,3 +5,11 @@ mod = Blueprint('main_page', __name__, template_folder='templates')
 @mod.route('/')
 def index():
     return render_template('main_site/index.html')
+
+@mod.route('/contact')
+def contact():
+    return render_template('main_site/contact.html')
+
+@mod.route('/about')
+def about():
+    return render_template('main_site/about.html')
